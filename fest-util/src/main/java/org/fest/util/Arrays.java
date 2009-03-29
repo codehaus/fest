6bdd778a33ccfc0e7180dc7cba05cfa0bf7214c2
alexruiz <alexruiz@0e018e5f-c563-0410-a877-f7675914c6cc>
2009-03-29 20:23:43 +0000

    RESOLVED - issue FEST-66: Deep equals for ObjectArrayAssert (issue 182) http://jira.codehaus.org/browse/FEST-66
    Method 'format(Object)' in org.fest.util.Arrays now supports formatting multi-dimensional arrays.

--- fest-util/src/main/java/org/fest/util/Arrays.java
+++ fest-util/src/main/java/org/fest/util/Arrays.java
@@ -1,24 +1,18 @@
 /*
  * Created on May 13, 2007
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- * 
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
  * Copyright @2007 the original author or authors.
  */
 package org.fest.util;
-
-import static org.fest.util.Strings.quote;
-
-import java.lang.reflect.Array;
-
 
 /**
  * Understands utility methods related to arrays.
@@ -26,6 +20,8 @@ import java.lang.reflect.Array;
  * @author Alex Ruiz
  */
 public class Arrays {
+
+  private static final ArrayFormatter formatter = new ArrayFormatter();
 
   /**
    * Returns <code>true</code> if the given array is <code>null</code> or empty.
@@ -43,28 +39,20 @@ public class Arrays {
    * @param values the values to store in the array.
    * @return an array containing the given arguments.
    */
-  public static <T> T[] array(T... values) { return values; }
+  public static <T> T[] array(T... values) {
+    return values;
+  }
 
   /**
-   * Returns the <code>String</code> representation of the given one-dimensional array, or <code>null</code> if the
-   * given object is either <code>null</code> or not a one-dimensional array.
-   * @param array the object that is expected to be a one-dimensional array.
-   * @return the <code>String</code> representation of the given one-dimensional array.
+   * Returns the <code>String</code> representation of the given array, or <code>null</code> if the
+   * given object is either <code>null</code> or not an array. This method supports arrays having other arrays as
+   * elements.
+   * @param array the object that is expected to be an array.
+   * @return the <code>String</code> representation of the given array.
    */
   public static String format(Object array) {
-    if (array == null) return null;
-    Class<?> type = array.getClass();
-    if (!type.isArray() || type.getComponentType().isArray()) return null;
-    int length = Array.getLength(array) - 1;
-    if (length == -1) return "[]";
-    StringBuilder b = new StringBuilder();
-    b.append('[');
-    for (int i = 0;; i++) {
-      b.append(quote(Array.get(array, i)));
-      if (i == length) return b.append(']').toString();
-      b.append(", ");
-    }
+    return formatter.format(array);
   }
-  
+
   private Arrays() {}
 }
