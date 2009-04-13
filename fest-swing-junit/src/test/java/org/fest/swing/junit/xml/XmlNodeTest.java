1daa432eb12f7f2f6700ff55bcf8131dacad9b58
alexruiz <alexruiz@0e018e5f-c563-0410-a877-f7675914c6cc>
2009-04-13 08:20:19 +0000

    IN PROGRESS - issue FEST-29: Taking Screenshots with GUITestRunner not possible with JUnit 4.4 (issue 129) http://jira.codehaus.org/browse/FEST-29
    Adding test more test cases.

--- fest-swing-junit/src/test/java/org/fest/swing/junit/xml/XmlNodeTest.java
+++ fest-swing-junit/src/test/java/org/fest/swing/junit/xml/XmlNodeTest.java
@@ -32,72 +32,118 @@ import org.w3c.dom.*;
  */
 @Test public class XmlNodeTest {
 
+  private Element target;
   private XmlNode node;
 
   @BeforeMethod public void setUp() throws Exception {
     Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-    Element e = document.createElement("person");
-    document.appendChild(e);
-    node = new XmlNode(e);
+    target = document.createElement("person");
+    document.appendChild(target);
+    node = new XmlNode(target);
+  }
+
+  public void shouldReturnTarget() {
+    assertThat(node.target()).isSameAs(target);
   }
 
   public void shouldAddNewChildWithoutAttributes() {
-    Element child = node.addNewNode("name").target();
-    assertThat(nameOf(child)).isEqualTo("name");
-    assertThat(parentOf(child)).isSameAs(node.target());
+    XmlNode newNode = node.addNewNode("new");
+    Element child = newNode.target();
+    assertThat(nameOf(child)).isEqualTo("new");
     assertThat(attributeCountOf(child)).isEqualTo(0);
+    assertThat(parentOf(child)).isSameAs(target);
   }
 
   public void shouldAddNewChildWithAttributes() {
-    Element child = node.addNewNode("name", attributes(name("first").value("Leia"),
-                                                       name("last").value("Organa"))).target();
-    assertThat(nameOf(child)).isEqualTo("name");
+    XmlNode newNode = node.addNewNode("new", attributes(name("name1").value("value1"), name("name2").value("value2")));
+    Element child = newNode.target();
+    assertThat(nameOf(child)).isEqualTo("new");
     assertThat(attributeCountOf(child)).isEqualTo(2);
-    assertThat(child.getAttribute("first")).isEqualTo("Leia");
-    assertThat(child.getAttribute("last")).isEqualTo("Organa");
-    assertThat(parentOf(child)).isSameAs(node.target());
+    assertThat(child.getAttribute("name1")).isEqualTo("value1");
+    assertThat(child.getAttribute("name2")).isEqualTo("value2");
+    assertThat(parentOf(child)).isSameAs(target);
   }
 
-  private static String nameOf(Element e) {
-    return e.getNodeName();
+  private static String nameOf(Node n) {
+    return n.getNodeName();
   }
 
-  private static Node parentOf(Element e) {
-    return e.getParentNode();
+  private static Node parentOf(Node n) {
+    return n.getParentNode();
   }
 
   public void shouldAddCDataNode() {
     node.addCdata("My CDATA");
-    Element target = node.target();
-    assertThat(childCountOf(target)).isEqualTo(1);
+    assertThat(childNodeCountOf(target)).isEqualTo(1);
     Node child = target.getFirstChild();
     assertThat(child).isInstanceOf(CDATASection.class);
-    CDATASection cdata = (CDATASection)child;
-    assertThat(cdata.getData()).isEqualTo("My CDATA");
+    assertThat(dataOf(child)).isEqualTo("My CDATA");
   }
 
   public void shouldAddTextNode() {
     node.addText("Hello World");
-    Element target = node.target();
-    assertThat(childCountOf(target)).isEqualTo(1);
+    assertThat(childNodeCountOf(target)).isEqualTo(1);
     Node child = target.getFirstChild();
     assertThat(child).isInstanceOf(Text.class);
-    Text text = (Text)child;
-    assertThat(text.getData()).isEqualTo("Hello World");
+    assertThat(dataOf(child)).isEqualTo("Hello World");
+  }
+
+  private static String dataOf(Node d) {
+    return ((CharacterData)d).getData();
   }
 
   public void shouldAddAttribute() {
     node.addAttribute(name("first").value("Leia"));
-    Element target = node.target();
     assertThat(attributeCountOf(target)).isEqualTo(1);
     assertThat(target.getAttribute("first")).isEqualTo("Leia");
   }
 
-  private int childCountOf(Element e) {
-    return e.getChildNodes().getLength();
+  private static int childNodeCountOf(Node n) {
+    return n.getChildNodes().getLength();
   }
 
-  private static int attributeCountOf(Element childTarget) {
-    return childTarget.getAttributes().getLength();
+  private static int attributeCountOf(Node n) {
+    return n.getAttributes().getLength();
+  }
+
+  public void shouldReturnParentNode() {
+    XmlNode newNode = node.addNewNode("new");
+    XmlNode parent = newNode.parentNode();
+    assertThat(parent).isEqualTo(node);
+    assertThat(parent.target()).isSameAs(target);
+  }
+
+  public void shouldReturnChildCount() {
+    node.addNewNode("one");
+    node.addNewNode("two");
+    assertThat(node.size()).isEqualTo(2);
+  }
+
+  public void shouldReturnChildAtPosition() {
+    XmlNode newNode = node.addNewNode("one");
+    assertThat(node.child(0)).isEqualTo(newNode);
+  }
+
+  public void shouldReturnNullChildAtPositionIsNotXmlElement() {
+    node.addText("Hello");
+    assertThat(node.child(0)).isNull();
+  }
+
+  public void shouldReturnAttributeValue() {
+    node.addAttribute(name("first").value("Leia"));
+    assertThat(node.valueOfAttribute("first")).isEqualTo("Leia");
+  }
+
+  public void shouldReturnNodeName() {
+    assertThat(node.name()).isEqualTo("person");
+  }
+
+  public void shouldReturnText() {
+    node.addText("Hello");
+    assertThat(node.text()).isEqualTo("Hello");
+  }
+
+  public void shouldOverrideToString() {
+    assertThat(node.toString()).isEqualTo("XmlNode[target=[person: null]]");
   }
 }
