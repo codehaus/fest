faf602a0f4db65fe1d60d44dba839defabf744bc
alexruiz <alexruiz@0e018e5f-c563-0410-a877-f7675914c6cc>
2009-04-28 06:33:45 +0000

    IN PROGRESS - issue FEST-29: Taking Screenshots with GUITestRunner not possible with JUnit 4.4 (issue 129) http://jira.codehaus.org/browse/FEST-29
    Minor cleanup.

--- fest-swing-junit/src/main/java/org/fest/swing/junit/ant/ImageFileWriter.java
+++ fest-swing-junit/src/main/java/org/fest/swing/junit/ant/ImageFileWriter.java
@@ -1,5 +1,5 @@
 /*
- * Created on Apr 5, 2009
+ * Created on Apr 27, 2009
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -15,17 +15,26 @@
  */
 package org.fest.swing.junit.ant;
 
+import static org.fest.swing.image.ScreenshotTaker.PNG_EXTENSION;
+import static org.fest.util.Files.newFile;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 /**
- * Understands common constants used in this package.
+ * Understands how to write an image as a file in the file system.
  *
  * @author Alex Ruiz
  */
-final class CommonConstants {
+class ImageFileWriter {
 
-  static final String UNKNOWN = "unknown";
-
-  static final String UTF_8 = "UTF-8";
-
-  private CommonConstants() {}
-
+  void saveAsPng(BufferedImage image, String path) throws IOException {
+    File newFile = new File(path);
+    if (newFile.exists()) return;
+    newFile = newFile(path);
+    ImageIO.write(image, PNG_EXTENSION, newFile);
+  }
 }
--- fest-swing-junit/src/main/java/org/fest/swing/junit/ant/ImageHandler.java
+++ fest-swing-junit/src/main/java/org/fest/swing/junit/ant/ImageHandler.java
@@ -1,38 +1,26 @@
 /*
  * Created on Jun 8, 2007
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2007-2009 the original author or authors.
  */
 package org.fest.swing.junit.ant;
 
 import static java.io.File.separator;
 import static java.util.logging.Level.SEVERE;
-import static java.util.logging.Level.WARNING;
-import static org.fest.swing.image.ScreenshotTaker.PNG_EXTENSION;
-import static org.fest.util.Files.flushAndClose;
-import static org.fest.util.Files.newFile;
 import static org.fest.util.Strings.isEmpty;
 
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
 import java.util.logging.Logger;
-
-import javax.imageio.ImageIO;
-
-import org.apache.commons.codec.binary.Base64;
 
 /**
  * Understands base64 encoding and decoding of an image.
@@ -41,52 +29,79 @@ import org.apache.commons.codec.binary.Base64;
  */
 public final class ImageHandler {
 
-  private static final String UTF_8 = "UTF-8";
   private static final String EMPTY_STRING = "";
 
   private static Logger logger = Logger.getAnonymousLogger();
-  
+
+  private static ImageEncoder imageEncoder = new ImageEncoder();
+  private static ImageDecoder imageDecoder = new ImageDecoder();
+  private static ImageFileWriter imageFileWriter = new ImageFileWriter();
+
+  /**
+   * Encodes the given image using the base64 algorithm. Failures in encoding an image are simply logged, no exceptions
+   * are thrown.
+   * @param image the image to encode.
+   * @return base64 characters.
+   */
   public static String encodeBase64(BufferedImage image) {
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    return encodeBase64(image, imageEncoder);
+  }
+
+  // makes testing easier
+  static String encodeBase64(BufferedImage image, ImageEncoder encoder) {
     try {
-      ImageIO.write(image, PNG_EXTENSION, out);
-      byte[] encoded = Base64.encodeBase64(out.toByteArray());
-      return new String(encoded, UTF_8);
-    } catch (IOException e) {
+      return encoder.encodeBase64(image);
+    } catch (Exception e) {
       logger.log(SEVERE, "Unable to encode image", e);
       return null;
-    } finally {
-      flushAndClose(out);
     }
   }
 
+  /**
+   * Decodes the given base64 characters into an image. Failures in decoding base64 characters are simply logged, no
+   * exceptions are thrown.
+   * @param encoded the given base64 characters.
+   * @return the decoded image.
+   */
   public static BufferedImage decodeBase64(String encoded) {
-    ByteArrayInputStream in = null;
-    try {
-      byte[] toDecode = encoded.getBytes(UTF_8);
-      in = new ByteArrayInputStream(Base64.decodeBase64(toDecode));
-      return ImageIO.read(in);
-    } catch (IOException e) {
-      logger.log(SEVERE, "Unable to decode image", e);
-      return null;
-    } 
+    return decodeBase64(encoded, imageDecoder);
   }
-  
-  public static String decodeBase64AndSaveAsPng(String encoded, String imageFilePath) {
-    if (isEmpty(encoded)) return EMPTY_STRING;
-    if (isEmpty(imageFilePath)) return EMPTY_STRING;
-    String realPath = imageFilePath.replace("/", separator);
-    BufferedImage image = decodeBase64(encoded);
-    File newFile = new File(realPath);
-    if (newFile.exists()) return EMPTY_STRING;
+
+  // makes testing easier
+  static BufferedImage decodeBase64(String encoded, ImageDecoder decoder) {
     try {
-      newFile = newFile(realPath);
-      ImageIO.write(image, PNG_EXTENSION, newFile);
+      return decoder.decodeBase64(encoded);
+    } catch (Exception e) {
+      logger.log(SEVERE, "Unable to encode image", e);
+      return null;
+    }
+  }
+
+  /**
+   * Decodes the given base64 characters into an image, and saves the decoded image as a file using the given path.
+   * Failures in decoding or saving the image as a file are simply logged, no exceptions are thrown.
+   * @param encoded the given base64 characters.
+   * @param path the path where to save the image file.
+   * @return empty <code>String</code>. This method is used by this extensions XSL stylesheets to decode the image in
+   * the XML report.
+   */
+  public static String decodeBase64AndSaveAsPng(String encoded, String path) {
+    return decodeBase64AndSaveAsPng(encoded, path, imageDecoder, imageFileWriter);
+  }
+
+  // makes testing easier
+  static String decodeBase64AndSaveAsPng(String encoded, String path, ImageDecoder decoder, ImageFileWriter writer) {
+    if (isEmpty(encoded)) return EMPTY_STRING;
+    if (isEmpty(path)) return EMPTY_STRING;
+    String realPath = path.replace("/", separator);
+    BufferedImage image = decodeBase64(encoded, decoder);
+    try {
+      writer.saveAsPng(image, realPath);
     } catch (Exception ignored) {
-      logger.log(WARNING, ignored.getMessage());
+      logger.log(SEVERE, ignored.getMessage());
     }
     return EMPTY_STRING;
   }
-  
+
   private ImageHandler() {}
 }
