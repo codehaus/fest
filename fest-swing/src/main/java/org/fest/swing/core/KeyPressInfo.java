b9171b18dc491d2965f4228783fdf09a0cf2442f
alexruiz <alexruiz@0e018e5f-c563-0410-a877-f7675914c6cc>
2009-06-06 10:38:08 +0000

    * Cleaning up code. * Adding more tests.

--- fest-swing/src/main/java/org/fest/swing/core/KeyPressInfo.java
+++ fest-swing/src/main/java/org/fest/swing/core/KeyPressInfo.java
@@ -1,26 +1,26 @@
 /*
  * Created on Sep 21, 2007
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2007-2009 the original author or authors.
  */
 package org.fest.swing.core;
+
+import static org.fest.swing.util.Arrays.copyOf;
 
 import java.awt.Event;
 import java.awt.event.KeyEvent;
 
 import org.fest.swing.util.Platform;
-
-import static org.fest.swing.util.Arrays.copyOf;
 
 /**
  * Understands information about pressing a keyboard key.
@@ -42,7 +42,7 @@ import static org.fest.swing.util.Arrays.copyOf;
  * </pre>
  * </p>
  * <p>
- * For platform-safe mask pressing (e.g. 'Control' in Windows or 'Command' in MacOS) use 
+ * For platform-safe mask pressing (e.g. 'Control' in Windows or 'Command' in MacOS) use
  * <code>{@link Platform#controlOrCommandMask()}</code>.
  * </p>
  *
@@ -53,9 +53,9 @@ public final class KeyPressInfo {
 
   private final int keyCode;
   private int[] modifiers;
-  
+
   /**
-   * Specifies the code of the key to press, without any modifiers (e.g. 
+   * Specifies the code of the key to press, without any modifiers (e.g.
    * <code>{@link KeyEvent#VK_C KeyEvent.VK_C}</code>.)
    * @param keyCode the code of the key to press.
    * @return the created <code>KeyPressInfo</code>.
@@ -63,7 +63,7 @@ public final class KeyPressInfo {
   public static KeyPressInfo keyCode(int keyCode) {
     return new KeyPressInfo(keyCode, new int[0]);
   }
-  
+
   private KeyPressInfo(int keyCode, int[] modifiers) {
     this.keyCode = keyCode;
     this.modifiers = modifiers;
@@ -74,21 +74,20 @@ public final class KeyPressInfo {
    * @return the code of the key to press.
    */
   public int keyCode() { return keyCode; }
-  
+
   /**
    * Returns the modifiers to use when pressing <code>{@link #keyCode the specified key}</code>.
    * @return the modifiers to use.
    */
   public int[] modifiers() {
-    if (modifiers == null) return new int[0];
     return copyOf(modifiers);
   }
-  
+
   /**
-   * Specifies the modifiers to use when pressing <code>{@link #keyCode the specified key}</code> (e.g. 
+   * Specifies the modifiers to use when pressing <code>{@link #keyCode the specified key}</code> (e.g.
    * <code>{@link Event#CTRL_MASK Event.CTRL_MASK}</code>.)
    * <p>
-   * For platform-safe mask pressing (e.g. 'Control' in Windows or 'Command' in MacOS) use 
+   * For platform-safe mask pressing (e.g. 'Control' in Windows or 'Command' in MacOS) use
    * <code>{@link Platform#controlOrCommandMask()}</code>.
    * </p>
    * @param newModifiers the new modifiers to use.
@@ -99,5 +98,5 @@ public final class KeyPressInfo {
     if (newModifiers == null) throw new NullPointerException("The array of modifiers should not be null");
     modifiers = copyOf(newModifiers);
     return this;
-  }  
+  }
 }
