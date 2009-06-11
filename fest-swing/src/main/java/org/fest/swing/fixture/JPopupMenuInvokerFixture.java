576bcf2b1667e1bb15555af24c822a7e268a890e
alexruiz <alexruiz@0e018e5f-c563-0410-a877-f7675914c6cc>
2009-06-11 19:47:51 +0000

    - Deprecated constructor 'TwoStateButtonFixture(Robot, Class)'. - Deprecated constructor 'JPopupMenuInvokerFixture(Robot, Class)'.

--- fest-swing/src/main/java/org/fest/swing/fixture/JPopupMenuInvokerFixture.java
+++ fest-swing/src/main/java/org/fest/swing/fixture/JPopupMenuInvokerFixture.java
@@ -1,19 +1,21 @@
 /*
  * Created on Mar 1, 2008
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2008-2009 the original author or authors.
  */
 package org.fest.swing.fixture;
+
+import static org.fest.swing.driver.ComponentStateValidator.validateIsEnabledAndShowing;
 
 import java.awt.Component;
 import java.awt.Point;
@@ -22,8 +24,6 @@ import javax.swing.JPopupMenu;
 
 import org.fest.swing.core.Robot;
 import org.fest.swing.exception.ComponentLookupException;
-
-import static org.fest.swing.driver.ComponentStateValidator.validateIsEnabledAndShowing;
 
 /**
  * Understands a fixture that can invoke a <code>{@link JPopupMenu}</code> on the target <code>{@link Component}</code>.
@@ -41,7 +41,10 @@ public abstract class JPopupMenuInvokerFixture<T extends Component> extends Comp
    * @throws NullPointerException if <code>type</code> is <code>null</code>.
    * @throws ComponentLookupException if a matching component could not be found.
    * @throws ComponentLookupException if more than one matching component is found.
+   * @deprecated use {@link #JPopupMenuInvokerFixture(Robot, Component)} instead, performing a component lookup by
+   * type first.
    */
+  @Deprecated
   public JPopupMenuInvokerFixture(Robot robot, Class<? extends T> type) {
     super(robot, type);
   }
--- fest-swing/src/main/java/org/fest/swing/fixture/TwoStateButtonFixture.java
+++ fest-swing/src/main/java/org/fest/swing/fixture/TwoStateButtonFixture.java
@@ -1,16 +1,16 @@
 /*
  * Created on Sep 18, 2007
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2007-2009 the original author or authors.
  */
 package org.fest.swing.fixture;
@@ -24,11 +24,11 @@ import org.fest.swing.exception.ComponentLookupException;
 /**
  * Understands simulation of user events on a two-state button and verification of the state of such button.
  * @param <T> the specific type of two-state button this fixture can handle.
- * 
+ *
  * @author Yvonne Wang
  * @author Alex Ruiz
  */
-public abstract class TwoStateButtonFixture<T extends AbstractButton> extends JPopupMenuInvokerFixture<T> 
+public abstract class TwoStateButtonFixture<T extends AbstractButton> extends JPopupMenuInvokerFixture<T>
     implements CommonComponentFixture, TextDisplayFixture {
 
   /**
@@ -39,7 +39,10 @@ public abstract class TwoStateButtonFixture<T extends AbstractButton> extends JP
    * @throws NullPointerException if <code>type</code> is <code>null</code>.
    * @throws ComponentLookupException if a matching component could not be found.
    * @throws ComponentLookupException if more than one matching component is found.
+   * @deprecated use {@link #TwoStateButtonFixture(Robot, AbstractButton)} instead, performing a component lookup by
+   * type first.
    */
+  @Deprecated
   public TwoStateButtonFixture(Robot robot, Class<? extends T> type) {
     super(robot, type);
   }
@@ -57,7 +60,7 @@ public abstract class TwoStateButtonFixture<T extends AbstractButton> extends JP
   public TwoStateButtonFixture(Robot robot, String name, Class<? extends T> type) {
     super(robot, name, type);
   }
-  
+
   /**
    * Creates a new <code>{@link TwoStateButtonFixture}</code>.
    * @param robot performs simulation of user events on the given <code>JToggleButton</code>.
