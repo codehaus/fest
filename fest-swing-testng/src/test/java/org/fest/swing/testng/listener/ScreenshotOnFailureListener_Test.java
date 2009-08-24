5762461f3e06b0a2c7ce1bd4ab42903343aaa601
alexruiz <alexruiz@0e018e5f-c563-0410-a877-f7675914c6cc>
2009-08-24 23:54:52 +0000

    * Refactoring test classes into smaller units. * Migrating tests from TestNG to JUnit.

--- fest-swing-testng/src/test/java/org/fest/swing/testng/listener/ScreenshotOnFailureListener_Test.java
+++ fest-swing-testng/src/test/java/org/fest/swing/testng/listener/ScreenshotOnFailureListener_Test.java
@@ -1,75 +1,76 @@
 /*
  * Created on May 16, 2007
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *
- * Copyright @2007-2009  the original author or authors.
+ * Copyright @2007-2009 the original author or authors.
  */
 package org.fest.swing.testng.listener;
-
-import java.awt.Toolkit;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-
-import org.testng.Reporter;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import org.fest.swing.annotation.GUITest;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.fest.assertions.ImageAssert.read;
 import static org.fest.util.Files.temporaryFolderPath;
-import static org.fest.util.Strings.*;
+import static org.fest.util.Strings.concat;
+import static org.fest.util.Strings.join;
+
+import java.awt.Toolkit;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import org.fest.swing.annotation.GUITest;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testng.Reporter;
 
 /**
  * Tests for <code>{@link ScreenshotOnFailureListener}</code>.
  *
  * @author Yvonne Wang
  */
-public class ScreenshotOnFailureListenerTest {
+public class ScreenshotOnFailureListener_Test {
 
-  @GUITest public static class SomeGUITestClass {
-    @GUITest public void someGUITestMethod() {}
+  @GUITest
+  public static class SomeGUITestClass {
+    @GUITest
+    public void someGUITestMethod() {}
+
     public void someNonGUITestMethod() {}
   }
 
-  private TestContextStub testContext;
-  private TestResultStub testResult;
+  static TestContextStub testContext;
+  static TestResultStub testResult;
+  static ScreenshotOnFailureListener listener;
 
-  private ScreenshotOnFailureListener listener;
-
-  @BeforeClass public void setUp() {
+  @BeforeClass
+  public static void setUpOnce() {
     testContext = new TestContextStub();
     testResult = new TestResultStub();
     listener = new ScreenshotOnFailureListener();
   }
 
-  @Test public void shouldGetOutputFolderOnStart() {
+  @Test
+  public void should_get_output_folder_on_start() {
     String outputFolder = temporaryFolderPath();
     testContext.setOutputDirectory(outputFolder);
     listener.onStart(testContext);
     assertThat(listener.output()).isEqualTo(outputFolder);
   }
 
-  @Test(dependsOnMethods = "shouldGetOutputFolderOnStart")
-  public void shouldTakeScreenshotOfDesktopOnTestFailure() throws Exception {
+  @Test
+  public void should_take_screenshot_on_test_failure() throws Exception {
     setUpStubsForScreenshot();
     listener.onTestFailure(testResult);
     String imageFileName = screenshotFileName();
     String screenshotPath = concat(testContext.getOutputDirectory(), imageFileName);
     assertThat(read(screenshotPath)).hasSize(Toolkit.getDefaultToolkit().getScreenSize());
-    assertScreenshotHyperlinkAddedToReport(imageFileName);
+    assertThatScreenshotHyperlinkWasAddedToReport(imageFileName);
   }
 
   private void setUpStubsForScreenshot() {
@@ -86,7 +87,7 @@ public class ScreenshotOnFailureListenerTest {
     return join(className, methodName, "png").with(".");
   }
 
-  private void assertScreenshotHyperlinkAddedToReport(String imageFileName) {
+  private void assertThatScreenshotHyperlinkWasAddedToReport(String imageFileName) {
     List<String> reporterOutput = Reporter.getOutput();
     assertThat(reporterOutput).hasSize(1);
     assertThat(reporterOutput.get(0)).isEqualTo(concat("<a href=\"", imageFileName, "\">Screenshot</a>"));
