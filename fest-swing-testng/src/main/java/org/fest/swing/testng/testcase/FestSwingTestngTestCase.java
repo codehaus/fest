c77b420f1d4b37d70b6b0fe344cb8275b517c905
alexruiz <alexruiz@0e018e5f-c563-0410-a877-f7675914c6cc>
2009-11-17 04:23:06 +0000

    Minor changes.

--- fest-swing-testng/src/main/java/org/fest/swing/testng/testcase/FestSwingTestngTestCase.java
+++ fest-swing-testng/src/main/java/org/fest/swing/testng/testcase/FestSwingTestngTestCase.java
@@ -1,16 +1,16 @@
 /*
  * Created on Jan 17, 2009
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Copyright @2009 the original author or authors.
  */
 package org.fest.swing.testng.testcase;
@@ -24,7 +24,7 @@ import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
 import org.fest.swing.testing.FestSwingTestCaseTemplate;
 
 /**
- * Understands a template for test cases that use FEST-Swing and TestNG. This template installs a 
+ * Understands a template for test cases that use FEST-Swing and TestNG. This template installs a
  * <code>{@link FailOnThreadViolationRepaintManager}</code> to verify violations of Swing thread rules and manages both
  * creation and clean up of a <code>{@link Robot}</code>.
  * @since 1.1
@@ -36,23 +36,23 @@ public abstract class FestSwingTestngTestCase extends FestSwingTestCaseTemplate 
   @BeforeClass public final void setUpOnce() {
     FailOnThreadViolationRepaintManager.install();
   }
-  
+
   @BeforeMethod public final void setUp() {
     setUpRobot();
     onSetUp();
   }
 
   /**
-   * Implementations set up their own test fixture in this method. This method is called after this test's 
-   * <code>{@link Robot}</code> has been created. 
+   * Implementations set up their own test fixture in this method. This method is called after this test's
+   * <code>{@link Robot}</code> has been created.
    */
   protected abstract void onSetUp();
-  
+
   @AfterMethod public final void tearDown() {
     try {
       onTearDown();
     } finally {
-      robot().cleanUp();
+      cleanUp();
     }
   }
 
